2c1b8d358fb467a2ea4359730682e160da02cdfa
Tiffany Han <hantiffany@gmail.com>
2017-08-31 14:43:37 -0700

    misc cleanup

--- linked_list/remove_item.js
+++ linked_list/remove_item.js
@@ -1,34 +1,3 @@
-function Node(data) {
-  this.val = data;
-  this.next = null;
-};
-
-function LinkedList() {
-  // head will be the top of the list
-  // we'll define it as null for now
-  this.head = null;
-  this.length = 0;
-
-    this.add = function(data) {
-    var nodeToAdd = new Node(data),
-        nodeToCheck = this.head;
-    // if the head is null
-    if(!nodeToCheck) {
-      this.head = nodeToAdd;
-      this.length++;
-      return nodeToAdd;
-    }
-    // loop until we find the end
-    while(nodeToCheck.next) {
-      nodeToCheck = nodeToCheck.next;
-    }
-    // once were at the end of the list
-    nodeToCheck.next = nodeToAdd;
-    this.length++;
-    return nodeToAdd;
-  }
-}
-
 // iterative approach, this iterates through 0..next to last node
 // each iteration, we check the next node to see if we want to delete
 // if yes, we change current.next to the one after
@@ -83,13 +52,13 @@ var removeElements = function(head, val) {
 // linkedList.add(6);
 // console.log(removeElements(linkedList.head, 6)); // [1,2,3,4,5]
 
-var linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(1);
-console.log(removeElements(linkedList.head, 1)) // []
+// var linkedList = new LinkedList();
+// linkedList.add(1);
+// linkedList.add(1);
+// console.log(removeElements(linkedList.head, 1)) // []
 
-var linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(1);
-linkedList.add(1);
-console.log(removeElements(linkedList.head, 1)) // []
+// var linkedList = new LinkedList();
+// linkedList.add(1);
+// linkedList.add(1);
+// linkedList.add(1);
+// console.log(removeElements(linkedList.head, 1)) // []
--- linked_list/remove_sorted_dups.js
+++ linked_list/remove_sorted_dups.js
@@ -1,34 +1,3 @@
-function Node(data) {
-  this.val = data;
-  this.next = null;
-};
-
-function LinkedList() {
-  // head will be the top of the list
-  // we'll define it as null for now
-  this.head = null;
-  this.length = 0;
-
-    this.add = function(data) {
-    var nodeToAdd = new Node(data),
-        nodeToCheck = this.head;
-    // if the head is null
-    if(!nodeToCheck) {
-      this.head = nodeToAdd;
-      this.length++;
-      return nodeToAdd;
-    }
-    // loop until we find the end
-    while(nodeToCheck.next) {
-      nodeToCheck = nodeToCheck.next;
-    }
-    // once were at the end of the list
-    nodeToCheck.next = nodeToAdd;
-    this.length++;
-    return nodeToAdd;
-  }
-}
-
 // iterative approach
 // how to use running pointer w/o index access?
 // this solution is almost identical to remove_item problem
@@ -55,16 +24,16 @@ var deleteDuplicates = function(head) {
 // [null,1,2]
 // current: 2, current.next: null
 
-var linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
+// var linkedList = new LinkedList();
+// linkedList.add(1);
+// linkedList.add(2);
 
-console.log(deleteDuplicates(linkedList)); // [1,2]
+// console.log(deleteDuplicates(linkedList)); // [1,2]
 
-linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(3);
-console.log(deleteDuplicates(linkedList)); // [1,2,3,4,5]
+// linkedList = new LinkedList();
+// linkedList.add(1);
+// linkedList.add(1);
+// linkedList.add(2);
+// linkedList.add(3);
+// linkedList.add(3);
+// console.log(deleteDuplicates(linkedList)); // [1,2,3,4,5]
