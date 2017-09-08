fd98f3df6d533989524509e44e2c553b351cc69f
Tiffany Han <hantiffany@gmail.com>
2017-09-08 16:01:20 -0700

    implement flatten_to_linked_list with postorder traversal solution

--- binary_tree/flatten_to_linked_list.js
+++ binary_tree/flatten_to_linked_list.js
@@ -12,6 +12,19 @@
  //    / \   \
  //   3   4   6
 
+ // recurse right
+ // recurse left
+ // process root
+
+ // left is solved: 2 3 4
+ // right is solved: 5 6
+ // how do i assemble the root?
+ // 2 is saved in left pointer
+ // 5 is saved in right pointer
+ // 1's left is null
+ // 1's right points to 2
+ // connect 2 to 5 6
+
  // 1
  //  \
  //   2
@@ -44,6 +57,9 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+
+// preorder traversal
+
 var flatten = function(root) {
   function recurse(root) {
     if (!root) return;
@@ -63,4 +79,35 @@ var flatten = function(root) {
 
   var prev = null;
   recurse(root);
+};
+
+// divide and conquer
+
+ //       1
+ //      / \
+ //     2   5
+ //    / \   \
+ //   3   4   6
+
+
+ //       1
+ //      /
+ //     2
+
+ // recurse right
+ // recurse left
+ // process root
+
+// postorder traversal, retains all links automatically. how?
+
+var flatten = function(root, memo={prevNode: null}) {
+  if (!root) return;
+
+  flatten(root.right, memo);
+  flatten(root.left,  memo);
+
+  root.left = null;
+  root.right = memo.prevNode;
+
+  memo.prevNode = root;
 };
