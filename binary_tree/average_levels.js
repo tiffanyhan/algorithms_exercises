680db05c4ddebd5e11486443dfeb056594bc552e
Tiffany Han <hantiffany@gmail.com>
2017-09-08 16:34:14 -0700

    implement average_levels without any built-in functions

--- binary_tree/average_levels.js
+++ binary_tree/average_levels.js
@@ -37,9 +37,86 @@ var averageOfLevels = function(root) {
     }
   }
 
-  return vals.map(function(level) {
-    return level.reduce(function(sum, val) {
-      return sum + val;
-    }, 0) / level.length;
-  });
+//   return vals.map(function(level) {
+//     return level.reduce(function(sum, val) {
+//       return sum + val;
+//     }, 0) / level.length;
+//   });
+// };
+
+// solution without built in methods
+// don't need to process root, bc the average is always just 1 value
+// just put root's children into the queue
+
+// at this point, we have 2 values in the queue
+// nothing in nextLevel
+
+// dequeue 9, process it (add it to levelTotal, increment length)
+// push its children into nextLevel (none)
+// dequeue 20, process it (add it to levelTotal, increment length)
+// push its children into nextLevel (15, 7)
+
+// when queue is empty, level is finished
+// take levelTotal and divide it by length of level
+
+// Input:
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+
+// Output:
+
+// [3, 14.5, 11]
+
+var averageOfLevels = function(root) {
+  var result    = [];
+  var queue     = [];
+  var nextLevel = [];
+  var currLevel = {total: 0, size: 0};
+  var current;
+
+  if (root) {
+    result.push(root.val);
+    if (root.left)  queue.push(root.left);
+    if (root.right) queue.push(root.right);
+  }
+
+  while (queue.length > 0) {
+    current = queue.shift();
+    currLevel.total += current.val;
+    currLevel.size++;
+
+    if (current.left)  nextLevel.push(current.left);
+    if (current.right) nextLevel.push(current.right);
+
+    if (queue.length === 0) {
+      result.push(currLevel.total / currLevel.size);
+      currLevel.total = 0;
+      currLevel.size  = 0;
+      queue = nextLevel;
+      nextLevel = [];
+    }
+  }
+
+  return result;
 };
+
+// function TreeNode(val) {
+//   this.val = val;
+//   this.left = this.right = null;
+// }
+
+// var node1 = new TreeNode(3);
+// var node2 = new TreeNode(9);
+// var node3 = new TreeNode(20);
+// var node4 = new TreeNode(15);
+// var node5 = new TreeNode(7);
+// node1.left = node2;
+// node1.right = node3;
+// node3.left = node4;
+// node3.right = node5;
+
+// console.log(averageOfLevels(node1));
