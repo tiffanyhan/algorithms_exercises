24161ddbf4f2969f1c820ac6addbd8b881ded76d
Tiffany Han <hantiffany@gmail.com>
2017-09-09 15:41:52 -0700

    add divide and conquer solution and explanation for is_valid_bst

--- binary_tree/is_valid_bst.rb
+++ binary_tree/is_valid_bst.rb
@@ -7,6 +7,53 @@ class TreeNode
   end
 end
 
+# true-
+#     2
+#    / \
+#   1   3
+
+# false-
+#     1
+#    / \
+#   2   3
+
+# is_valid 2, nil, nil
+
+# is_valid 1, nil, 2
+# on left subtree, val must be less than parent val (max)
+
+# is_valid 3, 2, nil
+# on right subtree, val must be greater than parent val (min)
+
+# true-
+#     5
+#    / \
+#   3   7
+#  /\   /\
+# 1  4  6 9
+
+# true-
+#     5
+#    / \
+#   3   7
+#  /\   /\
+# 1  6 1  9
+
+# is_valid root=5, min=nil, max=nil
+
+# is_valid root=3, min=nil, max=5
+# on left subtree, val must be less than parent val (max=5)
+
+# is_valid root=1, min=nil, max=3
+# on left subtree, val must be less than parent val (max=3)
+
+# is_valid root=4, min=3, max=5
+# on an inner tree, both conditionals will apply
+# val must be greater than min=3 and less than max=5
+
+# is_valid root=7, min=5, max=nil
+# on right subtree, val must be greater than parent val (min=5)
+
 # divide and conquer solution
 def is_valid_bst(root, min=nil, max=nil)
   return true if !root
